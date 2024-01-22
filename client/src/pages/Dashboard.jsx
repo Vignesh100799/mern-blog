b21429f8badc7eb3ffe0ce8c5874cd533d0fedc2
Vignesh100799 <baluvignesh422@gmail.com>
2024-01-22 12:22:19 +0530

    Tailwind added

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Button } from "flowbite-react";
+import React from "react";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div>
+      Dashboard
+      <Button className=" hover:text-black before:m-5 after:m-5">
+        Click me
+      </Button>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
