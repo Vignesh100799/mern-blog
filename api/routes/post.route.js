194c8f7c976f30a0add13209caf27034f9a61bdc
Vignesh100799 <baluvignesh422@gmail.com>
2024-01-25 17:37:57 +0530

    Get blog api created

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express"
 import verifyToken, { adminVerify } from "../utils/verifyUser.js"
-import createPost from "../controllers/post.controller.js"
+import createPost, { getBlogs } from "../controllers/post.controller.js"
 
 const router = express.Router()
 
 router.post("/create-blog",verifyToken,adminVerify,createPost)
+router.get("/get-blog",getBlogs)
 
 export default router
