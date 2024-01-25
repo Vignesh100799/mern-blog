c43d5dc5788ac4724dd8e51c522d7a4f75ce4e6d
Vignesh100799 <baluvignesh422@gmail.com>
2024-01-25 19:01:07 +0530

    Blog table added fro admin UI

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Table } from "flowbite-react";
+import { Link } from "react-router-dom";
 const DashPosts = () => {
-  return (
-    <div>DashPosts</div>
-  )
-}
+  const [blogs, setBlogs] = useState([]);
+  const { currentUser } = useSelector((state) => state.user);
 
-export default DashPosts
+  useEffect(() => {
+    const getBlogs = async () => {
+      try {
+        const res = await fetch(`/api/blog/get-blog?userId=${currentUser._id}`);
+
+        const data = await res.json();
+
+        if (res.ok) {
+          setBlogs(data.posts);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (currentUser.isAdmin) {
+      getBlogs();
+    }
+  }, [currentUser._id]);
+
+  return (
+    <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
+      {currentUser.isAdmin && blogs.length > 0 ? (
+        <Table hoverable className="shadow-md">
+          <Table.Head>
+            <Table.HeadCell>Date Uploaded</Table.HeadCell>
+            <Table.HeadCell>Post image</Table.HeadCell>
+            <Table.HeadCell>Post Title</Table.HeadCell>
+            <Table.HeadCell>Category</Table.HeadCell>
+            <Table.HeadCell>Delete</Table.HeadCell>
+            <Table.HeadCell>
+              <span>Edit</span>
+            </Table.HeadCell>
+          </Table.Head>
+          {blogs.map((posts) => (
+            <Table.Body key={posts._id}>
+              <Table.Row>
+                <Table.Cell>
+                  {new Date(posts.updatedAt).toLocaleDateString()}
+                </Table.Cell>
+                <Table.Cell>
+                  <Link to={`/blog/${posts.slug}`}>
+                    <img
+                      alt={posts.title}
+                      className="w-20 h-10 object-cover bg-gray-500"
+                      src={posts.image}
+                    />
+                  </Link>
+                </Table.Cell>
+                <Table.Cell>
+                  <Link to={`/blog/${posts.slug}`}>{posts.title}</Link>
+                </Table.Cell>
+                <Table.Cell>{posts.category}</Table.Cell>
+                <Table.Cell>
+                  <span className="font-medium text-red-500 hover:underline cursor-pointer">
+                    Delete
+                  </span>
+                </Table.Cell>
+                <Table.Cell>
+                  {" "}
+                  <Link
+                    className="text-teal-500 hover:underline"
+                    to={`/update-post/${posts._id}`}
+                  >
+                    <span>Edit</span>
+                  </Link>
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          ))}
+        </Table>
+      ) : (
+        <p>You have no posts yet!</p>
+      )}
+    </div>
+  );
+};
+
+export default DashPosts;
