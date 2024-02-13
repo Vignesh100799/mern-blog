395d2106ac1f9f967e3f587bcb4dba52810fc794
Vignesh100799 <baluvignesh422@gmail.com>
2024-02-13 06:20:32 +0530

    add preinstall

--- api/index.js
+++ api/index.js
@@ -8,7 +8,9 @@ import userRoute from "./routes/user.route.js"
 import authRoute from "./routes/auth.route.js"
 import postRoute from "./routes/post.route.js"
 import commnetRoute from "./routes/comment.route.js"
+import path from "path"
 
+const __dirname = path.resolve()
 const app = express()
 dotenv.config()
 db(process.env.DB_URL)
@@ -25,6 +27,11 @@ app.use("/api/user", userRoute)
 app.use("/api/auth", authRoute)
 app.use("/api/blog", postRoute)
 app.use("/api/comment", commnetRoute)
+app.use(express.static(path.join(__dirname, "/client/dist")))
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+}
+)
 
 app.use(errorHandler)
